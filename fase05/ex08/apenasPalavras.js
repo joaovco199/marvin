c25b02825f346d1b3009c511b603b63252a0e984
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-01-16 12:26:33 -0200

    apenas Palavras

---
+++ fase05/ex08/apenasPalavras.js
@@ -0,0 +1,16 @@
+function apenasPalavras(array) {
+    
+   n = 10000000000000000000000000000000000000000000000000000000000
+   
+    for (i  =0; i = n; array++) 
+    
+    x = typeof array
+    
+    return x
+    
+}
+
+
+
+
+
