9898b802c8f931b4f50f944da0d219d315e4fd0a
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-01-22 20:50:36 -0200

    somaPares

---
+++ fase06/ex04/somaPares.js
@@ -0,0 +1,15 @@
+function somaPares(n1, n2) {
+    
+var soma = 0
+         
+    for (i = n1; i <= n2; i++) {
+                  
+        if ( i % 2 === 0) { soma += i }
+    
+    }
+    
+return soma
+    
+}
+    
+  
