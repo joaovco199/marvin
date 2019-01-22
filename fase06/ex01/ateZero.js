d4cd8b2070d9ffbe63e0bcaf640ae33dbb87ec22
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-01-22 18:48:46 -0200

    ateZero

---
+++ fase06/ex01/ateZero.js
@@ -0,0 +1,25 @@
+function entreNumeros(num) {
+    
+    z = [];
+    
+    if (num < 0)  {x = num , y = 0}
+    
+    else x = 0, y = num;
+    
+for (i = x; i <= y; i++) {
+    
+    z.push(i);
+    
+}
+
+return z;
+  
+ }
+ 
+ 
+
+ 
+ 
+ 
+
+
