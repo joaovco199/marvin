2391f27a34be5ee3818f81f38d2536674de3595d
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-01-23 14:17:39 -0200

    meuNome

--- fase07/ex01/meuNome.js
+++ fase07/ex01/meuNome.js
@@ -1,14 +1,8 @@
 function meuNome(obj) {
     
-    obj = { 
-        
-        nome: "marvin",
-        
-        idade: 42
-        
-    }
-    
     return obj.nome.slice(0,1).toUpperCase() + obj.nome.slice(1).toLowerCase()
     
 }
+
+
 
