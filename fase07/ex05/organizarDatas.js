bbd08855b90710ea40fcfdfad7b0c2afe4241980
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-01-25 18:44:38 -0200

    Datas

--- fase07/ex05/organizarDatas.js
+++ fase07/ex05/organizarDatas.js
@@ -1,3 +1,21 @@
-function organizarDatas() {
+function organizarDatas(arr)  {
+        
+    x = arr.sort(function(a,b) {  
+        a = new Date(a.dataDeChegada)
+        b = new Date(b.dataDeChegada)
+                
+        if (a > b) 
+    {return 1}
+    
+    else if (a < b) return  -1
+    
+    else return 0
+
+    
+    })
+    
+    return x
     
 }
+
+
