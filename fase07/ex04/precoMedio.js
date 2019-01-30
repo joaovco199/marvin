041c42f406bea99bbc5694ea48dbac9bc362e974
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-01-30 17:35:32 -0200

    preco

--- fase07/ex04/precoMedio.js
+++ fase07/ex04/precoMedio.js
@@ -1,33 +1,22 @@
-function precoMedio() {
-    var array = []
-   var soma = 0 ;
-   var aspas = ""
-   
-  let arr = []  
-  
-  for (var i = 0; i < arguments.length; i++) {
-     arr.push(arguments[i])
-  }
-  
-   for (i=0; i < arr.length ; i++ ) {
+function precoMedio(...args) {
+        let soma = 0
+        let array = []
         
-        x = arr[i].preco ;
-        y = soma += x ;
-        z = array.push(x)
+        for (i=0; i < args.length; i++) {
             
+            x = args[i].preco
+            y = soma += x
+            z = array.push(x)
+        
         }
-      
-      var maior = Math.max.apply(null,array)
-      var index = array.indexOf(maior)
-      aspas= arr[index].nome
-      
-    console.log("O produto mais caro se chama  \"" + aspas + "\"")
+        
+    let maior = Math.max.apply(null, array)
+    let index = array.indexOf(maior)
+    let aspas = args[index].nome
+         
+    console.log("O produto mais caro se chama \"" + aspas + "\"")
     
-    return y/i;
-
+    return y/i
 }
-
-
-
 
 
