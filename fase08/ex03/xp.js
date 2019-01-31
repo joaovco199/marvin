83ed8e863b1ebba7c7a0145d217dce0544a1fee2
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-01-31 20:36:31 -0200

    xp

---
+++ fase08/ex03/xp.js
@@ -0,0 +1,13 @@
+function xp (num) {
+    xp1 = 100
+    nivel = 1
+    for ( i = 0; i < num; i++) {
+        if (num <= xp1){
+        return nivel
+        }
+    
+        else {xp1 += 100+ 50*nivel
+        nivel ++
+        }
+        }
+}
