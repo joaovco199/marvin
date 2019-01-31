7bb26a7089bd391a29f0b7d8229d06dfeba736a2
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-01-31 20:31:21 -0200

    bordas

--- fase08/ex06/bordas.js
+++ fase08/ex06/bordas.js
@@ -1,25 +1,24 @@
 function bordas(h, v) {
 if (h === 1) {h = 2; x = 0}
 else {x = 1}
-
+arr = []
     for (i = 1; i < h; i++) {
     
         while (i <= 1) {
-            console.log("A" + "B".repeat(h-2) + "C".repeat(x));
+            arr.push("A" + "B".repeat(h-2) + "C".repeat(x));
             i++;
         }
         
         while (i < v) {
-            console.log("B" + "O".repeat(h-2) + "B".repeat(x));
+            arr.push("B" + "O".repeat(h-2) + "B".repeat(x));
             i++;
         }
         
         if (i === v) {
-            console.log("C" + "B".repeat(h-2) + "A".repeat(x));
+            arr.push("C" + "B".repeat(h-2) + "A".repeat(x));
         }
         
     }
-    return i;
+    console.log( arr.join("\n"))
 }
 
-bordas (2,5);
