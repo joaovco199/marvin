f3e4aa50142143ebea9454f104540788890488e2
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-02-04 21:11:44 -0200

    Conversor

--- fase08/ex00/conversor.js
+++ fase08/ex00/conversor.js
@@ -5,25 +5,26 @@ function conversor( temp, de, para) {
     
     
     if (de == "celsius" && para == "kelvin") 
-    { return Math.ceil(temp + 273 )}
+    { return Math.ceil(temp + 273.15 )}
     
     else if (de == "celsius" && para == "fahrenheit")
-    {return Math.ceil(1,8*(temp) + 32)}
+    {return Math.ceil((temp * 9/5) + 32)}
     
     else if (de == "kelvin" && para == "celsius") 
-    { return Math.ceil(temp - 273 )}
+    { return Math.ceil(temp - 273.15 )}
     
     else if (de == "kelvin" && para == "fahrenheit") 
-    {return Math.ceil((75*temp) + 459.67)}
+    {return Math.ceil((temp - 273.15) * 9/5 + 32)}
     
     else if (de == "fahrenheit" && para == "celsius") 
     { return Math.ceil(((temp-32)/1.8))}
     
     else if (de == "fahrenheit" && para == "kelvin")
-    {return Math.ceil((75*temp + 459.67)*5/9)}
+    {return Math.ceil((temp - 32) * 5/9 + 273.15)}
     
     else return temp
 
 
 }
+
 
