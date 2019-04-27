f7edb519a8106e47f1d00ffba587931b0d079c3f
joaovco199 <40635645+joaovco199@users.noreply.github.com>
2019-04-27 16:06:29 -0300

    numeroAleatorio

---
+++ desafios/botao/numeroAleatorio.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+  <html lang="pt-BR" dir="ltr">
+    <head>
+      <meta charset="utf-8" />
+      <title>Numero aleatório</title>
+    </head>
+    <body>
+      <div id="container" onclick="numeroAleatorio()">0</div>
+    </body>
+  </html>
